768c7165160a0c25b387c1fb8bd76a78d65604c2
Kozhin Vladimir <affka@affka.ru>
2016-01-01 21:48:52 +0700

    auto-build comments

--- index-client.js
+++ index-client.js
@@ -1,5 +1,6 @@
 module.exports = require('jii');
 
+// AUTO-GENERATED Package files list
 require('./lib/client/ChangeEvent.js');
 require('./lib/client/Request.js');
 require('./lib/client/Response.js');
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('jii');
+// AUTO-GENERATED Package files list
+require('./lib/client/ChangeEvent.js');
+require('./lib/client/Request.js');
+require('./lib/client/Response.js');
+require('./lib/client/Router.js');
